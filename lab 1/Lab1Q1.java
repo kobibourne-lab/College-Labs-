da5226b9ce3032bd556d1af593e5d1362f9022bb
kobibourne-lab <kobibourne@gmail.com>
2025-09-22 18:30:29 +0100

    Lab 1 done

---
+++ lab 1/Lab1Q1.java
@@ -0,0 +1,21 @@
+// Student Name 	: Oisin Cawley
+// Student Id Number: 
+// Date 			: Nov-2015
+// Purpose 			: My first class implementation
+
+public class Lab1Q1
+{ // begin class ThermB
+	public static void main(String args[]) 
+	{ // being main method
+
+		Thermometer thermA = new Thermometer();		// Create an instance of our Thermometer class
+
+		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		thermA.setCelsius(20.0);
+		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		
+        Thermometer thermB = new Thermometer();		// Create an instance of our Thermometer class
+		thermB.setCelsius(10.0);
+		System.out.println("Temp. of Thermometer A is " + thermB.getCelsius() );
+	} // end main
+} // end class ThermTest
