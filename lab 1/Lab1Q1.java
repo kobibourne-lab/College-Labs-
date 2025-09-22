70ce88637aedc3db09b317bffbc50600ee642593
kobib <kobibourne@gmail.com>
2025-09-22 18:54:16 +0100

    Yes commit

--- lab 1/Lab1Q1.java
+++ lab 1/Lab1Q1.java
@@ -1,12 +1,12 @@
-// Student Name 	: Oisin Cawley
-// Student Id Number: 
-// Date 			: Nov-2015
+// Student Name 	: Kobi Bourne 
+// Student Id Number: C00XXXXXX
+// Date 			: Sept-22-2025
 // Purpose 			: My first class implementation
 
 public class Lab1Q1
 { // begin class ThermB
 	public static void main(String args[]) 
-	{ // being main method
+	{ // begin main method
 
 		Thermometer thermA = new Thermometer();		// Create an instance of our Thermometer class
 
@@ -14,7 +14,7 @@ public class Lab1Q1
 		thermA.setCelsius(20.0);
 		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
 		
-        Thermometer thermB = new Thermometer();		// Create an instance of our Thermometer class
+        Thermometer thermB = new Thermometer();		// Create a second instance of our Thermometer class
 		thermB.setCelsius(10.0);
 		System.out.println("Temp. of Thermometer A is " + thermB.getCelsius() );
 	} // end main
